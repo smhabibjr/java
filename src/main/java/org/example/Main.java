3358e6e8b0414dc7d1ce25e9332fd0b3fd4a883a
smhabibjr <smhabib.abir2030@gmail.com>
2024-02-29 18:27:55 +0100

    add java constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,15 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    int x = 1222;
-
-    static void myMethod(String msg){
-        System.out.println("this is my msg"+ msg);
+    String fullName;
+    int age;
+    public Main(String fname, int age) {
+        fullName = fname;
+        age = age;
     }
+
     public static void main(String[] args) {
-        System.out.println("hi  there");
-        Second calculator = new Second();
-        System.out.println(calculator.sum(10,20));
+        Main myMain = new Main("M Habib", 44);
+        System.out.println("Output for main class: " + myMain.fullName + " Age: " + myMain.age);
     }
 }
--- src/main/java/org/example/Second.java
+++ src/main/java/org/example/Second.java
@@ -3,15 +3,7 @@ package org.example;
 public class Second {
     public static void main(String[] args){
         System.out.println("this is second class ");
-        myMethod();
         System.out.println(sum(5,10));
-        Main myObject = new Main();
-        System.out.println(myObject.x);
-        myObject.myMethod("hi htere !");
-    }
-
-    private static void myMethod() {
-        System.out.println("i am here " + "Habib");
     }
 
     public static int sum(int num1, int num2){
